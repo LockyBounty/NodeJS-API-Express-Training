a68b6d14428ccf6ae229690f317e64ac06015e0f
locky <bounthienglocky@gmail.com>
2019-01-15 12:29:51 +0100

    reset

--- OLD/server.js
+++ OLD/server.js
@@ -3,7 +3,7 @@
 // BASE SETUP
 
 var mongoose   = require('mongoose');
-//mongoose.connect('mongodb://node:node@novus.modulusmongo.net:27017/Iganiq8o'); // connect to our database
+//mongoose.connect('mongodb:// node:node@novus. modulusmongo.net:27017/Iganiq8o'); // connect to our database //space added 
 
 
 // =============================================================================
